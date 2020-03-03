958060d9f973d383c228cff053080e26ebc9b97a
Kory Newton <korybnewton@gmail.com>
2020-03-02 16:49:18 -0800

    first chart, plot daily max temp over the last year

---
+++ MaxTempChart/chart.js
@@ -0,0 +1,88 @@
+async function drawLineChart() {
+  // import weather data
+  const dataset = await d3.json('./../../seattle_weather_data.json');
+  // const dataset = await d3.json('./../../kazakhstan_weather_data.json');
+
+  const yAccessor = d => d.temperatureMax;
+  const dateParser = d3.timeParse('%Y-%m-%d');
+  const xAccessor = d => dateParser(d.date);
+
+  // chart dimensions
+  let dimensions = {
+    width: window.innerWidth * 0.9,
+    height: 400,
+    margin: {
+      top: 15,
+      right: 15,
+      bottom: 40,
+      left: 60
+    }
+  };
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+  // chart canvas
+  const wrapper = d3
+    .select('#wrapper')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height);
+
+  // add group to wrapper that respects margins
+  const bounds = wrapper
+    .append('g')
+    .style(
+      'transform',
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
+
+  // y axis linear scaling using d3-scale module
+  // takes domain (inputs) and range (output) values into account
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.boundedHeight, 0]);
+
+  const freezingTemperaturePlacement = yScale(32);
+  const freezingTemperatures = bounds
+    .append('rect')
+    .attr('x', 0)
+    .attr('width', dimensions.boundedWidth)
+    .attr('y', freezingTemperaturePlacement)
+    .attr('height', dimensions.boundedHeight - freezingTemperaturePlacement)
+    .attr('fill', '#e0f3f3');
+
+  // scale x axis
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.boundedWidth]);
+
+  const lineGenerator = d3
+    .line()
+    .x(d => xScale(xAccessor(d)))
+    .y(d => yScale(yAccessor(d)));
+
+  // draws line from data in dataset
+  const line = bounds
+    .append('path')
+    .attr('d', lineGenerator(dataset))
+    .attr('fill', 'none')
+    .attr('stroke', '#af9358')
+    .attr('stroke-width', 2);
+
+  // generate and append y-axis group
+  const yAxisGenerator = d3.axisLeft().scale(yScale);
+  const yAxis = bounds.append('g').call(yAxisGenerator);
+
+  // generate and append x-axis group
+  const xAxisGenerator = d3.axisBottom().scale(xScale);
+  const xAxis = bounds
+    .append('g')
+    .call(xAxisGenerator)
+    .style('transform', `translateY(${dimensions.boundedHeight}px)`);
+}
+
+drawLineChart();
